53fc430217d620e9e057bb8d591535a62cc6f259
Rai Rossi <109825210+raissarossi@users.noreply.github.com>
2023-07-26 16:49:19 -0300

    feat: new home page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+interface Props {}
+
+const Home: NextPage<Props> = ({}) => {
+  return <div>
+    <text>Welcome to Oasis</text>
+  </div>
+}
+
+export default Home
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import Button from "@/components/Button";
-import { Modal } from "@/components/Modal";
 
 export default function Home() {
   return (
     <>
       <div className="bg-bosch-gray-550 h-screen">
-        <Modal title="Title Here" >
-          <>
-          Body Here
-          </>
-        </Modal>
-        <Button className="bg-bosch_green-7">Confirm</Button>
       </div>
     </>
   )
