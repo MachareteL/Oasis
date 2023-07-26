fff9f36ada12c85f9b9e7b6620e75e42f26b02fa
MachareteL <macharetelucas@gmail.com>
2023-07-26 16:22:12 -0300

    feat: :sparkles: NextAuth frist setup
    Including nextAuth, Providers directory contains the auth context, api/auth/[...nextauth]/route.ts is the api responsible for handling user authentication and lib contains global configs for prisma and nextauth lib

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import { BoschSansBold, BoschSansItalic, BoschSansRegular } from "@/fonts/fonts";
+import { BoschSansRegular } from "@/fonts/fonts";
 import "./globals.css";
 import type { Metadata } from "next";
-
+import { NextAuthProvider } from "./providers/providers";
 
 export const metadata: Metadata = {
-  title: 'Oasis',
-  description: 'A MVP PROJECT',
-}
+  title: "Oasis",
+  description: "A MVP PROJECT",
+};
 
 export default function RootLayout({
   children,
@@ -15,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={BoschSansRegular.className}>{children}</body>
+      <NextAuthProvider>
+        <body className={BoschSansRegular.className}>{children}</body>
+      </NextAuthProvider>
     </html>
   );
 }
