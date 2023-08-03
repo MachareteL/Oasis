24878bc9bf66b6ce6afebc076d0211722a7bce08
MachareteL <macharetelucas@gmail.com>
2023-08-03 11:03:58 -0300

    feat: :memo: raissa's changing my entire coding

--- src/components/Oasis.tsx
+++ src/components/Oasis.tsx
@@ -7,7 +7,7 @@ export default function Oasis() {
       <h1 className="text-bosch-gray-600 text-4xl font-extrabold dark:text-bosch-white ">
         BOSCH
       </h1>
-      <h1 className="text-bosch-gray-600 text-4xl font-bold dark:bg-gradient-to-r dark:from-oasis-standard-100 dark:to-oasis-standard-500 bg-clip-text dark:text-transparent">
+      <h1 className="text-bosch-gray-600 bg-clip-text text-4xl font-bold dark:bg-gradient-to-r dark:from-oasis-standard-100 dark:to-oasis-standard-500 dark:text-transparent">
         OASIS
       </h1>
     </span>
