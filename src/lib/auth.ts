c8193027c21357174b149a8684dc96cadee7d95b
MachareteL <macharetelucas@gmail.com>
2023-08-07 11:07:00 -0300

    feat: :tada: added bcrypt

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@ import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./prisma";
-
+import { compare, hash } from 'bcrypt'
 export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
@@ -23,6 +23,9 @@ export const authOptions: NextAuthOptions = {
         if (!credentials) {
           return null;
         }
+        const teste = await hash('lucas', 12)
+        console.log(teste);
+        
         const user = await prisma.user.findFirst({
           where: {
             email: credentials.email,
